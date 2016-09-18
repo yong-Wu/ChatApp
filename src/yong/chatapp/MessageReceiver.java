f3bff285d53dbbb257700a2c5f68aed8cb47ea83
罗洪 <442666876@qq.com>
2016-09-18 16:59:20 +0800

    test

--- src/yong/chatapp/MessageReceiver.java
+++ src/yong/chatapp/MessageReceiver.java
@@ -33,8 +33,8 @@ public class MessageReceiver extends BroadcastReceiver {
 	public static int mPushMsgNum = 0;
 	public static final int NOTIFY_ID = 0;
 	
-	BmobUserManager userManager;
-	BmobChatUser currentUser;
+	private BmobUserManager userManager;
+	private BmobChatUser currentUser;
 	
 	@Override
 	public void onReceive(Context context, Intent intent) {
